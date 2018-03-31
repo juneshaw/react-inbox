cbe817cb50cae99dd9987994fa5818f87f022e10
June Shaw <june.shaw@me.com>
2018-03-31 15:16:41 -0600

    Refactoring to put work in App.js and make Messages.js a container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,176 @@
 import React from 'react';
 import './App.css';
 import Messages from './components/Messages'
-
-// const messages =
-//   [
-//     {id: 1, text: "message1", read: true, starred: false, selected: false, labels:[{text: "dev"}, {text: "personal"}]},
-//     {id: 2, text: "message2", read: false, starred: true, selected: false, labels:[{text: "personal"}, {text: "gschool"}]},
-//     {id: 3, text: "message3", read: false, starred: false, selected: true, labels:[{text: "dev"}, {text: "gschool"}]}
-//   ]
+import { SELECTTYPE } from './components/Toolbar'
+import { selectedType } from './components/Toolbar'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      composeOpen: false};
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/messages`)
+    // const response = await fetch('/api/messages')
+    const json = await response.json()
+    this.setState(...this.state, {messages: json._embedded.messages})
+  }
+
+  findMessage = (id) => this.state.messages.findIndex((message) => (message.id === parseInt(id, 10)))
+
+
+  async patchMessage (requestBody) {
+    // const response = await fetch('/api/messages', {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/messages`, {
+        method: 'PATCH',
+        body: JSON.stringify(requestBody),
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+  }
+
+  async postMessage (requestBody) {
+    // const response = await fetch('/api/messages', {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/messages`, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+    const json = await response.json()
+    const message = {
+      id: json.id,
+      subject: json.subject,
+      body: json.body,
+      read: json.read,
+      starred: json.starred,
+      labels: json.labels
+    }
+    this.setState({...this.state, messages:[...this.state.messages, message]})
+  }
+
+  handleMessageChange = (request) => {
+    let updatedMessages = [...this.state.messages]
+    let messageIndex = this.findMessage(request.currentTarget.dataset.id)
+    if (messageIndex > -1) {
+      let message = updatedMessages[messageIndex]
+      let command = request.currentTarget.id
+      switch (request.currentTarget.id) {
+        case 'star': {
+          message.starred = !message.starred
+          this.patchMessage({command,  "messageIds": [message.id], "star": message.starred})
+          break
+        }
+        case 'select': {
+          message.selected = !message.selected
+          break
+        }
+        case 'read': {
+          if (!message.read) {
+            message.read = true
+            this.patchMessage({command,  "messageIds": [message.id], "read": message.read})
+          }
+          break
+        }
+        default: break
+      }
+      this.setState({...this.state, messages:updatedMessages})
+    }
+  }
+
+  selectedMessageIds = (messages) => messages.filter = (message) => message.selected
+
+  handleOpenCompose = () => {
+    this.setState({...this.state, "composeOpen": !this.state.composeOpen})
+  }
+
+  buildRequest = (target) => ({subject: target.subject.value, body: target.body.value})
+
+  resetComposeForm = () => {
+    document.getElementById('subject').value=""
+    document.getElementById('body').value=""
+  }
+
+  handleCompose = (event) => {
+    event.preventDefault()
+    const request = this.buildRequest(event.target)
+    this.postMessage(request)
+    this.resetComposeForm()
+    this.setState({...this.state, "composeOpen": false})
+  }
+
+  handleToolbarChange = (request) => {
+    let updatedMessages
+    let updateState = true
+    let messageIds = this.state.messages.filter((message) => message.selected).map((message) => message.id)
+    const command = request.currentTarget.id
+    switch (command) {
+      case "select_messages": {
+        updatedMessages = this.state.messages.map((message) =>
+          (selectedType(this.state.messages) === SELECTTYPE.NONE ||
+          selectedType(this.state.messages) === SELECTTYPE.SOME) ?
+          {...message, selected: true} :
+          {...message, selected: false})
+        break
+      }
+      case "mark_as_read": {
+        updatedMessages = this.state.messages.map((message) =>
+           message.selected  ? {...message, read: true} : message)
+        this.patchMessage({command: "read",  messageIds, "read": true})
+        break
+      }
+      case "mark_as_unread": {
+        updatedMessages = this.state.messages.map((message) =>
+           message.selected  ? {...message, read: false} : message)
+        this.patchMessage({command: "read",  messageIds, "read": false})
+        break
+      }
+      case "apply_label": {
+        let newLabel = request.currentTarget.value
+        updatedMessages = this.state.messages.map((message) => {
+          if (message.selected && message.labels.findIndex((label) => (label === newLabel)) < 0) {
+            message.labels.push(newLabel)
+          }
+          return {...message, labels: message.labels}
+        })
+        this.patchMessage({command: "addLabel", messageIds, "label": newLabel})
+        break
+      }
+      case "remove_label": {
+        let newLabel = request.currentTarget.value
+        updatedMessages = this.state.messages.map((message) => {
+          if (message.selected) {
+            let index = message.labels.findIndex((label) => (label === newLabel))
+            if (index >= 0) {
+              message.labels.splice(index, 1)
+            }
+          }
+          return {...message, labels: message.labels}
+        })
+        this.patchMessage({command: "removeLabel", messageIds, "label": newLabel})
+        break
+      }
+      case "delete": {
+        updatedMessages = this.state.messages.filter((message) => {
+          return (!message.selected)
+        })
+        this.patchMessage({command: "delete", messageIds})
+        break
+      }
+      default: {
+        updateState = false
+        break
+      }
+    }
+    if (updateState) {
+      this.setState({...this.state, messages: updatedMessages})
+    }
+
+  }
 
   render() {
     return (
@@ -18,8 +179,12 @@ class App extends React.Component {
           <h1 className="App-title">React Inbox</h1>
         </header>
         <Messages
-          messages={[]}
-          composeOpen={false}
+          messages={this.state.messages}
+          composeOpen={this.state.composeOpen}
+          handleToolBarChange={this.handleToolbarChange}
+          handleMessageChange={this.handleMessageChange}
+          handleOpenCompose={this.handleOpenCompose}
+          handleCompose={this.handleCompose}
         />
       </div>
     );
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,201 +1,31 @@
 import React from 'react'
 import Toolbar from './Toolbar'
-import { SELECTTYPE } from './Toolbar'
-import { selectedType } from './Toolbar'
 import Message from './Message'
 import ComposeForm from './ComposeForm'
 
-class Messages extends React.Component {
+const Messages = ({messages, composeOpen, handleToolBarChange, handleMessageChange, handleOpenCompose, handleCompose}) => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      messages: props.messages,
-      composeOpen: props.composeOpen};
-  }
-
-  async componentDidMount() {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/messages`)
-    // const response = await fetch('/api/messages')
-    const json = await response.json()
-    this.setState(...this.state, {messages: json._embedded.messages})
-  }
-
-  findMessage = (id) => this.state.messages.findIndex((message) => (message.id === parseInt(id, 10)))
-
-
-  async patchMessage (requestBody) {
-    // const response = await fetch('/api/messages', {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/messages`, {
-        method: 'PATCH',
-        body: JSON.stringify(requestBody),
-        headers: {
-            'Content-Type': 'application/json'
-        }})
-  }
-
-  async postMessage (requestBody) {
-    // const response = await fetch('/api/messages', {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/messages`, {
-        method: 'POST',
-        body: JSON.stringify(requestBody),
-        headers: {
-            'Content-Type': 'application/json'
-        }})
-    const json = await response.json()
-    const message = {
-      id: json.id,
-      subject: json.subject,
-      body: json.body,
-      read: json.read,
-      starred: json.starred,
-      labels: json.labels
-    }
-    this.setState({...this.state, messages:[...this.state.messages, message]})
-  }
-
-  handleMessageChange = (request) => {
-    let updatedMessages = [...this.state.messages]
-    let messageIndex = this.findMessage(request.currentTarget.dataset.id)
-    if (messageIndex > -1) {
-      let message = updatedMessages[messageIndex]
-      let command = request.currentTarget.id
-      switch (request.currentTarget.id) {
-        case 'star': {
-          message.starred = !message.starred
-          this.patchMessage({command,  "messageIds": [message.id], "star": message.starred})
-          break
-        }
-        case 'select': {
-          message.selected = !message.selected
-          break
-        }
-        case 'read': {
-          if (!message.read) {
-            message.read = true
-            this.patchMessage({command,  "messageIds": [message.id], "read": message.read})
-          }
-          break
-        }
-        default: break
-      }
-      this.setState({...this.state, messages:updatedMessages})
-    }
-  }
-
-  selectedMessageIds = (messages) => messages.filter = (message) => message.selected
-
-  handleOpenCompose = () => {
-    this.setState({...this.state, "composeOpen": !this.state.composeOpen})
-  }
-
-  buildRequest = (target) => ({subject: target.subject.value, body: target.body.value})
-
-  resetComposeForm = () => {
-    document.getElementById('subject').value=""
-    document.getElementById('body').value=""
-  }
-
-  handleCompose = (event) => {
-    event.preventDefault()
-    const request = this.buildRequest(event.target)
-    this.postMessage(request)
-    this.resetComposeForm()
-    this.setState({...this.state, "composeOpen": false})
-  }
-
-  handleToolbarChange = (request) => {
-    let updatedMessages
-    let updateState = true
-    let messageIds = this.state.messages.filter((message) => message.selected).map((message) => message.id)
-    const command = request.currentTarget.id
-    switch (command) {
-      case "select_messages": {
-        updatedMessages = this.state.messages.map((message) =>
-          (selectedType(this.state.messages) === SELECTTYPE.NONE ||
-          selectedType(this.state.messages) === SELECTTYPE.SOME) ?
-          {...message, selected: true} :
-          {...message, selected: false})
-        break
-      }
-      case "mark_as_read": {
-        updatedMessages = this.state.messages.map((message) =>
-           message.selected  ? {...message, read: true} : message)
-        this.patchMessage({command: "read",  messageIds, "read": true})
-        break
-      }
-      case "mark_as_unread": {
-        updatedMessages = this.state.messages.map((message) =>
-           message.selected  ? {...message, read: false} : message)
-        this.patchMessage({command: "read",  messageIds, "read": false})
-        break
-      }
-      case "apply_label": {
-        let newLabel = request.currentTarget.value
-        updatedMessages = this.state.messages.map((message) => {
-          if (message.selected && message.labels.findIndex((label) => (label === newLabel)) < 0) {
-            message.labels.push(newLabel)
-          }
-          return {...message, labels: message.labels}
-        })
-        this.patchMessage({command: "addLabel", messageIds, "label": newLabel})
-        break
-      }
-      case "remove_label": {
-        let newLabel = request.currentTarget.value
-        updatedMessages = this.state.messages.map((message) => {
-          if (message.selected) {
-            let index = message.labels.findIndex((label) => (label === newLabel))
-            if (index >= 0) {
-              message.labels.splice(index, 1)
-            }
-          }
-          return {...message, labels: message.labels}
-        })
-        this.patchMessage({command: "removeLabel", messageIds, "label": newLabel})
-        break
-      }
-      case "delete": {
-        updatedMessages = this.state.messages.filter((message) => {
-          return (!message.selected)
-        })
-        this.patchMessage({command: "delete", messageIds})
-        break
-      }
-      default: {
-        updateState = false
-        break
-      }
-    }
-    if (updateState) {
-      this.setState({...this.state, messages: updatedMessages})
-    }
-
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <Toolbar
-          messages={this.state.messages}
-          toolbarHandler={this.handleToolbarChange}
-          openComposeHandler={this.handleOpenCompose}
-        />
-        <ComposeForm
-          composeHandler={this.handleCompose}
-          composeOpen={this.state.composeOpen}
-        />
-        {this.state.messages.map((message, i) => {
-          return (
-            <Message
-              key={i}
-              message={message}
-              messagesHandler={this.handleMessageChange}/>
-          )
-        })}
-      </div>
-    )
-  }
+  return (
+    <div className="container">
+      <Toolbar
+        messages={messages}
+        toolbarHandler={handleToolBarChange}
+        openComposeHandler={handleOpenCompose}
+      />
+      <ComposeForm
+        composeHandler={handleCompose}
+        composeOpen={composeOpen}
+      />
+      {messages.map((message, i) => {
+        return (
+          <Message
+            key={i}
+            message={message}
+            messagesHandler={handleMessageChange}/>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Messages
